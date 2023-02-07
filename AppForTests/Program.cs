86c31602eb9fb54f1985fb2f33b5a694ae5197dd
Aleksey Potapov <6254773@gmail.com>
2023-02-07 17:18:29 +0300

    Add Console Project  with Tests

---
+++ AppForTests/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AppForTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Source String:");
+
+            var sourceString = @"ä'á`č-ď_ěéíľľňôóřŕšťúůýž ÄÁČĎĚÉÍĽĽŇÔÓŘŔŠŤÚŮÝŽ ÖÜË łŁđĐ ţŢşŞçÇ øı";
+            Console.WriteLine(sourceString);
+
+            var R1 = Helper.RemoveDiacritics(sourceString);
+
+            Console.WriteLine(R1);
+
+            var r2 = Helper.RemoveDiacriticsRegExp(sourceString);
+            Console.WriteLine(r2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SampleApp.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using AppForTests;
+
+namespace SampleApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var sourceString = @"ä'á`č-ď_ěéíľľňôóřŕšťúůýž ÄÁČĎĚÉÍĽĽŇÔÓŘŔŠŤÚŮÝŽ ÖÜË łŁđĐ ţŢşŞçÇ øı";
+            var result = Helper.RemoveDiacritics(sourceString);
+            var expected = @"a'a`c-d_eeillnoorrstuuyz AACDEEILLNOORRSTUUYZ OUE łŁđĐ tTsScC øı";
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
